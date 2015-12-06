16e5349e944d431b9d0f2f8a14636be94d87c9a2
Jennifer Han <jhan2@andrew.cmu.edu>
2015-12-05 19:41:52 -0500

    initial commit

--- DGCalendarTutorial/AppDelegate.swift
+++ DGCalendarTutorial/AppDelegate.swift
@@ -7,17 +7,36 @@
 //
 
 import UIKit
+import Parse
+import Bolts
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // [Optional] Power your app with Local Datastore. For more info, go to
+        // https://parse.com/docs/ios_guide#localdatastore/iOS
+        Parse.enableLocalDatastore()
+        
+        // Initialize Parse.
+        Parse.setApplicationId("mrjY1mxxCoQLsjJsNKfeMUODwyuN33M1dH9u1yJK",
+            clientKey: "MklthA1w8YfkoSSpy2A0yzFhlJYU28V7QoXHgtZt")
+        
+        // [Optional] Track statistics around application opens.
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        
+        // ...
+        return true
+    }
+    
 
     var window: UIWindow?
 
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
-    }
+//    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+//        // Override point for customization after application launch.
+//        return true
+//    }
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- DGCalendarTutorial/ViewController.swift
+++ DGCalendarTutorial/ViewController.swift
@@ -7,19 +7,94 @@
 //
 
 import UIKit
+import EventKit
+import Parse
 
 class ViewController: UIViewController {
-
+    
+    var savedEventId : String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Testing parse set up 
+        let testObject = PFObject(className: "Event")
+        testObject["EventName"] = "End Of Finals"
+        testObject["hostedBy"] = "The Students of CMU"
+        testObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+            print("Object has been saved.")
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    // Creates an event in the EKEventStore. The method assumes the eventStore is created and
+    // accessible
+    func createEvent(eventStore: EKEventStore, title: String, startDate: NSDate, endDate: NSDate) {
+        let event = EKEvent(eventStore: eventStore)
+        
+        event.title = title
+        event.startDate = startDate
+        event.endDate = endDate
+        event.calendar = eventStore.defaultCalendarForNewEvents
+        do {
+            try eventStore.saveEvent(event, span: .ThisEvent)
+            savedEventId = event.eventIdentifier
+        } catch {
+            print("Bad things happened")
+        }
+    }
+    
+    // Removes an event from the EKEventStore. The method assumes the eventStore is created and
+    // accessible
+    func deleteEvent(eventStore: EKEventStore, eventIdentifier: String) {
+        let eventToRemove = eventStore.eventWithIdentifier(eventIdentifier)
+        if (eventToRemove != nil) {
+            do {
+                try eventStore.removeEvent(eventToRemove!, span: .ThisEvent)
+            } catch {
+                print("Bad things happened")
+            }
+        }
+    }
+    
+    // Responds to button to add event. This checks that we have permission first, before adding the
+    // event
+    @IBAction func addEvent(sender: UIButton) {
+        let eventStore = EKEventStore()
+        
+        let startDate = NSDate()
+        let endDate = startDate.dateByAddingTimeInterval(60 * 60) // One hour
+        
+        if (EKEventStore.authorizationStatusForEntityType(.Event) != EKAuthorizationStatus.Authorized) {
+            eventStore.requestAccessToEntityType(.Event, completion: {
+                granted, error in
+                self.createEvent(eventStore, title: "Grapevine Event!", startDate: startDate, endDate: endDate)
+            })
+        } else {
+            createEvent(eventStore, title: "Grapevine Event!", startDate: startDate, endDate: endDate)
+        }
+    }
+    
+    
+    // Responds to button to remove event. This checks that we have permission first, before removing the
+    // event
+    @IBAction func removeEvent(sender: UIButton) {
+        let eventStore = EKEventStore()
+        
+        if (EKEventStore.authorizationStatusForEntityType(.Event) != EKAuthorizationStatus.Authorized) {
+            eventStore.requestAccessToEntityType(.Event, completion: { (granted, error) -> Void in
+                self.deleteEvent(eventStore, eventIdentifier: self.savedEventId)
+            })
+        } else {
+            deleteEvent(eventStore, eventIdentifier: savedEventId)
+        }
+        
+    }
 }
 
